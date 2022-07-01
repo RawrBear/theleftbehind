a69b1b117ef8603dfd585726813ebbb7c1c1fe69
RawrBear <rawrbearmedia@gmail.com>
2022-07-01 16:42:25 -0400

    Cleaned up Files. This is the new initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import Navigation from "./components/Navigation";
-
 function App() {
     return (
         <div className="App">
-            <Navigation />
             <h1>TEST</h1>
         </div>
     );
