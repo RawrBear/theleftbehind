4e7f5af24bc26f628a949d155b5e2dc08c0e259d
RawrBear <rawrbearmedia@gmail.com>
2022-07-01 20:08:20 -0400

    Added links and styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                Cinzel: ["Cinzel", "serif"],
+                Martel: ["Martel", "serif"],
+            },
+        },
     },
     plugins: [require("daisyui")],
 };
