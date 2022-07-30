621a1093a8b6ef465d1619cde286e603a0f10636
RawrBear <rawrbearmedia@gmail.com>
2022-07-30 16:48:16 -0400

    Added intro text

--- src/components/IntroText/IntroText.jsx
+++ src/components/IntroText/IntroText.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 
 export const IntroText = () => {
     return (
@@ -6,30 +6,36 @@ export const IntroText = () => {
             <div className="w-1/2 py-16 space-y-4">
                 <h1 className="pb-6 text-center">Mass Global Disappearance</h1>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Maiores quis aut reiciendis. Tempore ab quos dolores
-                    delectus ducimus voluptatibus earum culpa exercitationem
-                    quam! Nihil, doloribus? Error pariatur totam rerum sunt.
+                    This site is here as a resource to those who are left behind
+                    after the mass disappearance. I'm not sure how they will say
+                    it happened, some theories say that it will be alien
+                    abduction, other people say it will be some kind of virus
+                    tha kills people. Either way, they are lying.
                 </p>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Maiores quis aut reiciendis. Tempore ab quos dolores
-                    delectus ducimus voluptatibus earum culpa exercitationem
-                    quam! Nihil, doloribus? Error pariatur totam rerum sunt.
+                    This mass vanishing of people is an event called the
+                    "rapture" which is written about in the Bible. If you're not
+                    religious, this will sound insane and like a made up story.
+                    This is why I put this site together, to show you, step by
+                    step what is coming, and when you've seen it for yourself,
+                    my hope is that you will see the truth.
                 </p>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Maiores quis aut reiciendis. Tempore ab quos dolores
-                    delectus ducimus voluptatibus earum culpa exercitationem
-                    quam! Nihil, doloribus? Error pariatur totam rerum sunt.
+                    You might be here before this event though as we don't know
+                    when it's coming. Maybe someone sent you here because they
+                    want you to gain more knowledge, maybe they're hoping you'll
+                    be saved.. Whatever brought you here wants better things for
+                    you. At the bottom of the page you will find the Gospel.
+                    This is THE MOST IMPORTANT PART so if you're not interested
+                    in the timeline, just skip to that part, read it and
+                    hopefully it will get you thinking.
                 </p>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Maiores quis aut reiciendis. Tempore ab quos dolores
-                    delectus ducimus voluptatibus earum culpa exercitationem
-                    quam! Nihil, doloribus? Error pariatur totam rerum sunt.
+                    I hope this site helps you in some way, even if its just to
+                    get you to think outside the box for a minute. Either way, I
+                    hope you find what you're looking for.
                 </p>
             </div>
         </div>
-    );
-};
+    )
+}
